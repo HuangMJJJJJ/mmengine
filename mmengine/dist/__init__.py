8a92d28a2a493a58fc1df1e163f638d5d4009a9e
huangmengjie <mengjie.huang@ai2robotics.com>
2024-05-14 06:39:57 +0000

    whl ignore

--- mmengine/dist/__init__.py
+++ mmengine/dist/__init__.py
@@ -1,8 +1,15 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .dist import (all_gather_object, all_reduce, all_gather, all_reduce_dict,
-                   collect_results, gather, broadcast, gather_object,
-                   sync_random_seed, broadcast_object_list,
-                   collect_results_cpu, collect_results_gpu, all_reduce_params)
+import torch.distributed
+if torch.distributed.is_available():
+    from .dist import (all_gather_object, all_reduce, all_gather, all_reduce_dict,
+                    collect_results, gather, broadcast, gather_object,
+                    sync_random_seed, broadcast_object_list,
+                    collect_results_cpu, collect_results_gpu, all_reduce_params)
+else:
+    (all_gather_object, all_reduce, all_gather, all_reduce_dict,
+                    collect_results, gather, broadcast, gather_object,
+                    sync_random_seed, broadcast_object_list,
+                    collect_results_cpu, collect_results_gpu, all_reduce_params) = [None] * 13
 from .utils import (get_dist_info, init_dist, init_local_group, get_backend,
                     get_world_size, get_rank, get_local_size, get_local_rank,
                     is_main_process, master_only, barrier, get_local_group,
